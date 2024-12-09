793f46a0727289369e06f2f6bb960c80415447fc
naganuma <naganuma.tsutomu@kk-protect.co.jp>
2024-12-09 16:55:15 +0900

    update: setting

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( "IS_VITE_DEVELOPMENT", true ); // develop mode
+define( "IS_DEVELOPMENT", true ); // develop mode
 
 // Initializing the Theme's Core Classes
 require_once(__DIR__ . '/functions/class-my-theme.php');
@@ -8,7 +8,7 @@ $MT = new My_Theme();
 $MT->init();
 
 // Development mode only functions
-if ( defined( 'IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT === true ) {
+if ( defined( 'IS_DEVELOPMENT') && IS_DEVELOPMENT === true ) {
     require_once(__DIR__ . '/functions/modules/dev.php');
 }
 
--- src/functions/hooks/class-head.php
+++ src/functions/hooks/class-head.php
@@ -11,15 +11,9 @@ class Head
 
     public function styles_and_scripts()
     {
+        // CSS
         wp_enqueue_style('main-style', get_theme_file_uri() . '/assets/css/style.css');
-        // サイト全体でのJS
-        wp_enqueue_script('main-js', get_theme_file_uri('/assets/js/main.js'), array(), null, true);
-
-        //------------------------------------------  WordPress 本体の jQuery を登録解除
-        // wp_deregister_script('jquery');
-
-        // ------------------------------------------ jQueryを読み込む
-        // wp_enqueue_script('jquery', get_theme_file_uri('/assets/js/lib/jquery-3.6.0.min.js'), array(), '3.6.0', true);
-
+        // JS
+        wp_enqueue_script('main-js', get_theme_file_uri('/assets/js/main.js'), array('jquery'), null, true);
     }
 }
