fbab11fadaf1e3c592fa61c9030f8ccc710a94a7
naganuma <naganuma.tsutomu@kk-protect.co.jp>
2024-12-18 17:18:05 +0900

    fix: remove contactform7 p

--- src/functions.php
+++ src/functions.php
@@ -13,3 +13,9 @@ if ( defined( 'IS_DEVELOPMENT') && IS_DEVELOPMENT === true ) {
 }
 
 // ------------------------------ ここから追加 ------------------------------
+
+// Contact Form 7で自動挿入されるPタグ、brタグを削除
+add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
+function wpcf7_autop_return_false() {
+  return false;
+}
