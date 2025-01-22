63374ac565db6b640b998b61b8e31f159f2b7b2d
naganuma <naganuma.tsutomu@kk-protect.co.jp>
2025-01-22 15:54:50 +0900

    fix: contactform7 設置固定ページにID付与

--- src/templates/page/page-order.php
+++ src/templates/page/page-order.php
@@ -1,4 +1,4 @@
-<div class="contact-form">
+<div class="contact-form" id="order_form">
     <div class="contact-form__title">
         <h2 class="contact-form__title_main">見積り依頼フォーム</h2>
         <span class="contact-form__title_sub">お見積もりは無料です。お気軽にお問い合わせください。</span>
